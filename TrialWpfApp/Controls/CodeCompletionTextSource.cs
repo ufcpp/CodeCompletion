2a01b8dd31c854ca324d4f761dea46b326e9691e
Nobuyuki Iwanaga <ufcpp@live.jp>
2024-11-22 15:48:18 +0900

    GenericTextRunProperties キャッシュ

--- TrialWpfApp/Controls/CodeCompletionTextSource.cs
+++ TrialWpfApp/Controls/CodeCompletionTextSource.cs
@@ -31,31 +31,51 @@ internal class CodeCompletionTextSource(SemanticModel semantics, CommonTextPrope
     {
         var buffer = semantics.Texts;
 
-        if (textSourceCharacterIndex > buffer.TotalLength) return new TextEndOfParagraph(1);
+        if (textSourceCharacterIndex > buffer.TotalLength) return _end;
 
         var (token, pos) = buffer.GetPosition(textSourceCharacterIndex);
         var array = buffer.Tokens[token].ArraySegment;
 
-        if (array.Count == 0) return new TextCharacters(" ", new GenericTextRunProperties(Brushes.Black, textRunProperties)); ;
-        if (array.Count == pos) return new TextCharacters(" ", new GenericTextRunProperties(Brushes.Black, textRunProperties));
+        if (array.Count == 0) return new TextCharacters(" ", GetTextRunProperties(null));
+        if (array.Count == pos) return new TextCharacters(" ", GetTextRunProperties(null));
 
         var props = GetTextRunProperties(token);
         return new TextCharacters(array.Array, 0, array.Count, props);
     }
 
+    private static readonly TextEndOfParagraph _end = new(1);
+
     private GenericTextRunProperties GetTextRunProperties(int token)
     {
         var node = semantics.Nodes.ElementAtOrDefault(token);
-        var brush = node switch
-        {
-            PropertyNode => Brushes.MidnightBlue,
-            PrimitivePropertyNode => Brushes.DarkGreen,
-            CompareNode => Brushes.DimGray,
-            LiteralNode => Brushes.DarkRed,
-            KeywordNode => Brushes.Blue,
-            _ => Brushes.Black,
-        };
-        var props = new GenericTextRunProperties(brush, textRunProperties);
-        return props;
+        return GetTextRunProperties(node);
+    }
+
+    private static readonly Brush[] _nodeBrushes =
+    [
+        Brushes.Black,
+        Brushes.MidnightBlue,
+        Brushes.DarkGreen,
+        Brushes.DimGray,
+        Brushes.DarkRed,
+        Brushes.Blue,
+    ];
+
+    private static int GetBrushIndex(Node? node) => node switch
+    {
+        PropertyNode => 1,
+        PrimitivePropertyNode => 2,
+        CompareNode => 3,
+        LiteralNode => 4,
+        KeywordNode => 5,
+        _ => 0,
+    };
+
+    private readonly GenericTextRunProperties?[] _runProps = new GenericTextRunProperties[_nodeBrushes.Length];
+
+    private GenericTextRunProperties GetTextRunProperties(Node? node)
+    {
+        var i = GetBrushIndex(node);
+        return _runProps[i] ?? new(_nodeBrushes[i], textRunProperties);
     }
 }
