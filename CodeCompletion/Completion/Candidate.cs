03e40a12eb722a733f8b27aa18d5fc529f81b467
Nobuyuki Iwanaga <ufcpp@live.jp>
2024-11-29 21:03:31 +0900

    候補造るところ分離

--- CodeCompletion/Completion/Candidate.cs
+++ CodeCompletion/Completion/Candidate.cs
@@ -1,3 +1,5 @@
+using CodeCompletion.Text;
+
 namespace CodeCompletion.Completion;
 
 /// <summary>
@@ -8,3 +10,135 @@ public record struct Candidate(string? Text);
 //todo:
 // Description みたいなの出したい。
 // 整数・文字列リテラルみたいな自由入力にもヒントくらいは出したい。
+
+internal static class Candidates
+{
+    public static IEnumerable<Candidate> GetCandidates(ReadOnlySpan<char> previousToken, PropertyHierarchy property)
+    {
+        var cat = Tokenizer.Categorize(previousToken);
+        if (cat == TokenCategory.Operator)
+        {
+            if (property.Direct.PropertyType == typeof(bool))
+            {
+                return _boolValues;
+            }
+            // nullable
+
+            else return [new(null)]; //todo: 何型の自由入力かのヒントくらいは出せるようにしたい。
+        }
+        if (cat == TokenCategory.DotIntrinsics && Intrinsic(previousToken) is { } c)
+        {
+            return c;
+        }
+        if (cat == TokenCategory.Isolation)
+        {
+            // ) の後ろはリテラルとかと同じ扱い。
+            if (previousToken is ")") return _conjunction;
+
+            // ,|&( の後ろは前の ( 直前のプロパティを元に候補を出す。
+            return Property(property.Parenthesis);
+        }
+        if (previousToken is [] // ルート。他の判定方法の方がいいかも…
+        || cat == TokenCategory.Identifier)
+        {
+            return Property(property.Direct);
+        }
+
+        return _conjunction;
+
+        //todo: 色々移植。
+    }
+
+    private static readonly Candidate[] _conjunction =
+    [
+        new(","),
+        new("|"),
+        new("&"),
+    ];
+
+    public static IEnumerable<Candidate>? Intrinsic(ReadOnlySpan<char> token) => token switch
+    {
+        IntrinsicNames.Length => _stringCandidates,
+        IntrinsicNames.Ceiling
+        or IntrinsicNames.Floor
+        or IntrinsicNames.Round => _comparableCandidates,
+        //todo: .any .all
+        _ => null,
+    };
+
+    public static IEnumerable<Candidate> Property(Property property)
+    {
+        //todo: enumerable
+
+        return PrimitiveProperty(property.PropertyType)
+            ?? property.PropertyType.GetProperties().Select(p => new Candidate(p.Name)).Append(new("("));
+    }
+
+    private static Candidate[]? PrimitiveProperty(Type type)
+    {
+        if (type == typeof(string)) return _stringCandidates;
+        if (type == typeof(bool)) return _boolCandidates;
+        if (type == typeof(float)
+            || type == typeof(double)
+            || type == typeof(decimal)
+            ) return _floatCandidates;
+        if (type == typeof(int)
+            || type == typeof(long)
+            || type == typeof(short)
+            || type == typeof(byte)
+            || type == typeof(uint)
+            || type == typeof(ulong)
+            || type == typeof(ushort)
+            || type == typeof(sbyte)
+            || type == typeof(TimeSpan)
+            || type == typeof(DateTime)
+            || type == typeof(DateTimeOffset)
+            || type == typeof(DateOnly)
+            || type == typeof(TimeOnly)
+            ) return _comparableCandidates;
+
+        //todo: nullable
+        //todo: enum
+
+        //todo: IComparable かつ ISpanParseable
+        //todo: IEquatable かつ ISpanParseable
+
+        return null;
+    }
+
+    private static readonly Candidate[] _boolValues = [new("true"), new("false")];
+
+    private static readonly Candidate[] _boolCandidates =
+    [
+        new("="),
+        new("!="),
+        // bool にも一応 ( 出す？
+        // その場合、任意の equatable と同じ扱いなので _equatableCandidates って名前にする？
+    ];
+
+    private static readonly Candidate[] _comparableCandidates =
+    [
+        new("="),
+        new("!="),
+        new("<"),
+        new("<="),
+        new(">"),
+        new(">="),
+        new("("), // 末尾に持っていきたい
+    ];
+
+    private static readonly Candidate[] _stringCandidates =
+    [
+        .._comparableCandidates,
+        new("~"),
+        new(IntrinsicNames.Length),
+    ];
+
+    private static readonly Candidate[] _floatCandidates =
+    [
+        .._comparableCandidates,
+        new(IntrinsicNames.Ceiling),
+        new(IntrinsicNames.Floor),
+        new(IntrinsicNames.Round),
+    ];
+}
--- CodeCompletion/Completion/CompletionContext.cs
+++ CodeCompletion/Completion/CompletionContext.cs
@@ -17,6 +17,10 @@ public class CompletionContext
 
     private readonly List<PropertyHierarchy> _propertyInfo = [];
 
+    /// <summary>
+    /// 全更新。
+    /// todo: 1ストロークごとに更新処理掛けれないか要検討。
+    /// </summary>
     public void Refresh()
     {
         var t = Root;
@@ -78,118 +82,6 @@ public class CompletionContext
         return null;
     }
 
-    private static IEnumerable<Candidate> GetCandidates(ReadOnlySpan<char> previousToken, PropertyHierarchy property)
-    {
-        var cat = Tokenizer.Categorize(previousToken);
-        if (cat == TokenCategory.Operator)
-        {
-            if (property.Direct.PropertyType == typeof(bool))
-            {
-                return [new("true"), new("false")];
-            }
-            // nullable
-
-            else return [new(null)]; //todo: 何型の自由入力かのヒントくらいは出せるようにしたい。
-        }
-        if (cat == TokenCategory.DotIntrinsics)
-        {
-            return previousToken switch
-            {
-                IntrinsicNames.Length => _stringCandidates,
-                IntrinsicNames.Ceiling
-                or IntrinsicNames.Floor
-                or IntrinsicNames.Round => _comparableCandidates,
-                _ => [],
-            };
-        }
-        if (cat == TokenCategory.Isolation)
-        {
-            // ) の後ろはリテラルとかと同じ扱い。
-            if (previousToken is ")") return _conjunction;
-
-            // ,|&( の後ろは前の ( 直前のプロパティを元に候補を出す。
-            return GetPropertyCandidates(property.Parenthesis);
-        }
-        if (previousToken is [] // ルート。他の判定方法の方がいいかも…
-        || cat == TokenCategory.Identifier)
-        {
-            return GetPropertyCandidates(property.Direct);
-        }
-
-        return _conjunction;
-
-        //todo: 色々移植。
-    }
-
-    private static IEnumerable<Candidate> GetPropertyCandidates(Property property)
-    {
-        if (property.PropertyType == typeof(string)) return _stringCandidates;
-        if (property.PropertyType == typeof(bool)) return _boolCandidates;
-        if (property.PropertyType == typeof(float)
-            || property.PropertyType == typeof(double)
-            || property.PropertyType == typeof(decimal)
-            ) return _floatCandidates;
-        if (property.PropertyType == typeof(int)
-            || property.PropertyType == typeof(long)
-            || property.PropertyType == typeof(short)
-            || property.PropertyType == typeof(byte)
-            || property.PropertyType == typeof(uint)
-            || property.PropertyType == typeof(ulong)
-            || property.PropertyType == typeof(ushort)
-            || property.PropertyType == typeof(sbyte)
-            || property.PropertyType == typeof(TimeSpan)
-            || property.PropertyType == typeof(DateTime)
-            || property.PropertyType == typeof(DateTimeOffset)
-            || property.PropertyType == typeof(DateOnly)
-            || property.PropertyType == typeof(TimeOnly)
-            ) return _comparableCandidates;
-        //todo: IComparable かつ ISpanParseable
-        //todo: IEquatable かつ ISpanParseable
-
-        return property.PropertyType.GetProperties().Select(p => new Candidate(p.Name)).Append(new("("));
-    }
-
-    private static readonly Candidate[] _conjunction =
-    [
-        new(","),
-        new("|"),
-        new("&"),
-    ];
-
-    private static readonly Candidate[] _boolCandidates =
-    [
-        new("="),
-        new("!="),
-        // bool にも一応 ( 出す？
-        // その場合、任意の equatable と同じ扱いなので _equatableCandidates って名前にする？
-    ];
-
-    private static readonly Candidate[] _comparableCandidates =
-    [
-        new("="),
-        new("!="),
-        new("<"),
-        new("<="),
-        new(">"),
-        new(">="),
-        new("("), // 末尾に持っていきたい
-    ];
-
-    private static readonly Candidate[] _stringCandidates =
-    [
-        .._comparableCandidates,
-        new("~"),
-        new(IntrinsicNames.Length),
-    ];
-
-    private static readonly Candidate[] _floatCandidates =
-    [
-        .._comparableCandidates,
-        new(IntrinsicNames.Ceiling),
-        new(IntrinsicNames.Floor),
-        new(IntrinsicNames.Round),
-    ];
-
     public void GetCandidates(IList<Candidate> results)
     {
         results.Clear();
@@ -197,7 +89,7 @@ public class CompletionContext
         var previousToken = pos == 0 ? "" : Texts.Tokens[pos - 1].Span;
         var text = Texts.Tokens[pos].Span;
 
-        foreach (var candidate in GetCandidates(previousToken, _propertyInfo[pos]))
+        foreach (var candidate in Candidates.GetCandidates(previousToken, _propertyInfo[pos]))
         {
             if (candidate.Text is not { } ct
                 || ct.AsSpan().StartsWith(text, StringComparison.OrdinalIgnoreCase))
