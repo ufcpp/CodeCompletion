5f5505dc7dfe93f58ec1fda775ce55f577e12fbf
Nobuyuki Iwanaga <ufcpp@live.jp>
2024-11-29 17:16:48 +0900

    string がうまく処理できないようになってた

--- CodeCompletion/TypeHelper.cs
+++ CodeCompletion/TypeHelper.cs
@@ -4,10 +4,13 @@ internal class TypeHelper
 {
     public static Type? GetElementType(Type t)
     {
+        if (t == typeof(string)) return null;
+
         if (t.IsArray) return t.GetElementType();
 
         // 無条件 IEnumerable でいい？
         // もうちょっと絞る？
+        // 実際、 string を誤検知する(のでメソッド先頭に特殊分岐がある)。
         foreach (var i in t.GetInterfaces())
         {
             if (i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)) return i.GetGenericArguments()[0];
