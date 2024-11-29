36489af17825166d19562733fb616440dc6edc8a
Nobuyuki Iwanaga <ufcpp@live.jp>
2024-11-30 00:35:54 +0900

    .key 案(todo コメントのみ)

--- CodeCompletion/Completion/IntrinsicToken.cs
+++ CodeCompletion/Completion/IntrinsicToken.cs
@@ -9,4 +9,7 @@ internal class IntrinsicNames
     public const string Round = ".round";
     public const string Any = ".any";
     public const string All = ".all";
+
+    // KeyValuePair は TValue 扱い(.Value を展開)にした上で、.Key の参照に .key intrinsic を用意するとかありかも。
+    // その方が dictionary に対する条件書きやすく。
 }
