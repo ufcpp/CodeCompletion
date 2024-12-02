58254b96a0c018c82fa42721df18becb507f8a76
Nobuyuki Iwanaga <ufcpp@live.jp>
2024-12-02 16:57:40 +0900

    enum Emit

--- CodeCompletion/Emit/ObjectMatcher.Compare.cs
+++ CodeCompletion/Emit/ObjectMatcher.Compare.cs
@@ -1,5 +1,6 @@
 using CodeCompletion.Completion;
 using CodeCompletion.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace CodeCompletion.Emit;
 
@@ -63,23 +64,32 @@ internal static class Compare
     {
         public static ObjectMatcher? Create(ComparisonType comparison, Type type, ReadOnlySpan<char> valueSpan)
         {
+            Type? genericType = null;
+
+            if (type.IsEnum)
+            {
+                genericType = typeof(CompreEnum<,>).MakeGenericType(type, type.GetEnumUnderlyingType());
+            }
+
             var i = TypeHelper.HasInterface(type);
             if (i.HasFlag(InterfaceType.ISpanParsable | InterfaceType.IComparable))
             {
-                var t = typeof(ComparableParseable<>).MakeGenericType(type);
-                var m = t.GetMethod(nameof(ComparableParseable<int>.Create), System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)!;
-                var f = m.CreateDelegate<Func<ComparisonType, ReadOnlySpan<char>, ObjectMatcher?>>();
-                return f(comparison, valueSpan);
+                genericType = typeof(ComparableParseable<>).MakeGenericType(type);
             }
             else if (i.HasFlag(InterfaceType.ISpanParsable | InterfaceType.IEquatable))
             {
-                var t = typeof(EquatableParseable<>).MakeGenericType(type);
-                var m = t.GetMethod(nameof(ComparableParseable<int>.Create), System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)!;
-                var f = m.CreateDelegate<Func<ComparisonType, ReadOnlySpan<char>, ObjectMatcher?>>();
-                return f(comparison, valueSpan);
+                genericType = typeof(EquatableParseable<>).MakeGenericType(type);
             }
 
-            return null;
+            return InvokeCreate(comparison, valueSpan, genericType);
+        }
+
+        private static ObjectMatcher? InvokeCreate(ComparisonType comparison, ReadOnlySpan<char> valueSpan, Type? t)
+        {
+            if (t is null) return null;
+            var m = t.GetMethod("Create", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)!;
+            var f = m.CreateDelegate<Func<ComparisonType, ReadOnlySpan<char>, ObjectMatcher?>>();
+            return f(comparison, valueSpan);
         }
     }
 
@@ -109,35 +119,12 @@ internal static class Compare
             _ => throw new NotImplementedException() // 他は null 返して単に無視してるのが多い
         };
 
-        private class Equal(T operand) : ObjectMatcher<T>
-        {
-            public override bool Match(T value) => value.CompareTo(operand) == 0;
-        }
-
-        private class NotEqual(T operand) : ObjectMatcher<T>
-        {
-            public override bool Match(T value) => value.CompareTo(operand) != 0;
-        }
-
-        private class GreaterThan(T operand) : ObjectMatcher<T>
-        {
-            public override bool Match(T value) => value.CompareTo(operand) > 0;
-        }
-
-        private class GreaterThanOrEqual(T operand) : ObjectMatcher<T>
-        {
-            public override bool Match(T value) => value.CompareTo(operand) >= 0;
-        }
-
-        private class LessThan(T operand) : ObjectMatcher<T>
-        {
-            public override bool Match(T value) => value.CompareTo(operand) < 0;
-        }
-
-        private class LessThanOrEqual(T operand) : ObjectMatcher<T>
-        {
-            public override bool Match(T value) => value.CompareTo(operand) <= 0;
-        }
+        private class Equal(T operand) : ObjectMatcher<T> { public override bool Match(T value) => value.CompareTo(operand) == 0; }
+        private class NotEqual(T operand) : ObjectMatcher<T> { public override bool Match(T value) => value.CompareTo(operand) != 0; }
+        private class GreaterThan(T operand) : ObjectMatcher<T> { public override bool Match(T value) => value.CompareTo(operand) > 0; }
+        private class GreaterThanOrEqual(T operand) : ObjectMatcher<T> { public override bool Match(T value) => value.CompareTo(operand) >= 0; }
+        private class LessThan(T operand) : ObjectMatcher<T> { public override bool Match(T value) => value.CompareTo(operand) < 0; }
+        private class LessThanOrEqual(T operand) : ObjectMatcher<T> { public override bool Match(T value) => value.CompareTo(operand) <= 0; }
     }
 
     private class EquatableParseable<T>
@@ -162,14 +149,47 @@ internal static class Compare
             _ => throw new NotImplementedException() // 他は null 返して単に無視してるのが多い
         };
 
-        private class Equal(T operand) : ObjectMatcher<T>
-        {
-            public override bool Match(T value) => value.Equals(operand);
-        }
+        private class Equal(T operand) : ObjectMatcher<T> { public override bool Match(T value) => value.Equals(operand); }
+        private class NotEqual(T operand) : ObjectMatcher<T> { public override bool Match(T value) => !value.Equals(operand); }
+    }
 
-        private class NotEqual(T operand) : ObjectMatcher<T>
+    private class CompreEnum<TEnum, TUnderlying>
+        where TEnum : struct, Enum
+        where TUnderlying : IComparable<TUnderlying>, ISpanParsable<TUnderlying>
+    {
+        public static ObjectMatcher? Create(ComparisonType comparison, ReadOnlySpan<char> valueSpan)
         {
-            public override bool Match(T value) => !value.Equals(operand);
+            valueSpan = StringHelper.Unescape(valueSpan); // Date 系、"" で囲まないと入力できない。
+
+            // 名前で parse
+            if (!Enum.TryParse<TEnum>(valueSpan, out var value))
+            {
+                // 数値で parse
+                if (TUnderlying.TryParse(valueSpan, null, out var x)) value = Unsafe.BitCast<TUnderlying, TEnum>(x);
+                else return null;
+            }
+            return Comparable.Create(comparison, value);
         }
+    }
+
+    private class Comparable
+    {
+        public static ObjectMatcher Create(ComparisonType comparison, IComparable operand) => comparison switch
+        {
+            ComparisonType.Equal => new Equal(operand),
+            ComparisonType.NotEqual => new NotEqual(operand),
+            ComparisonType.GreaterThan => new GreaterThan(operand),
+            ComparisonType.GreaterThanOrEqual => new GreaterThanOrEqual(operand),
+            ComparisonType.LessThan => new LessThan(operand),
+            ComparisonType.LessThanOrEqual => new LessThanOrEqual(operand),
+            _ => throw new NotImplementedException() // 他は null 返して単に無視してるのが多い
+        };
+
+        private class Equal(IComparable operand) : ObjectMatcher<IComparable> { public override bool Match(IComparable value) => value.CompareTo(operand) == 0; }
+        private class NotEqual(IComparable operand) : ObjectMatcher<IComparable> { public override bool Match(IComparable value) => value.CompareTo(operand) != 0; }
+        private class GreaterThan(IComparable operand) : ObjectMatcher<IComparable> { public override bool Match(IComparable value) => value.CompareTo(operand) > 0; }
+        private class GreaterThanOrEqual(IComparable operand) : ObjectMatcher<IComparable> { public override bool Match(IComparable value) => value.CompareTo(operand) >= 0; }
+        private class LessThan(IComparable operand) : ObjectMatcher<IComparable> { public override bool Match(IComparable value) => value.CompareTo(operand) < 0; }
+        private class LessThanOrEqual(IComparable operand) : ObjectMatcher<IComparable> { public override bool Match(IComparable value) => value.CompareTo(operand) <= 0; }
     }
 }
